2015857c4ffbe5c694212b068c2b847b0fdc6673
wmatous <willmatous@gmail.com>
2019-05-27 22:04:18 -0700

    procfile

--- pbeta2/urls.py
+++ pbeta2/urls.py
@@ -40,8 +40,8 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
     url(r'^$', home, name='home'),
 ]
